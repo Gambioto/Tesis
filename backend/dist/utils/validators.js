d01bba75281a22489d86a59732f4b83f1b516186
Gambioto <gerafeve@gmail.com>
2024-11-14 20:54:52 -0300

    corregido validator

--- backend/dist/utils/validators.js
+++ backend/dist/utils/validators.js
@@ -1,18 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.signupValidator = exports.validate = void 0;
-const express_1 = require("express");
 const express_validator_1 = require("express-validator");
 const validate = (validations) => {
     return async (req, res, next) => {
         for (let validation of validations) {
-            const result = await validation.run(express_1.Request);
+            const result = await validation.run(req);
             if (!result.isEmpty()) {
                 break;
             }
         }
         const errors = (0, express_validator_1.validationResult)(req);
-        if (errors.isEmpty) {
+        if (errors.isEmpty()) {
             return next();
         }
         return res.status(422).json({ errors: errors.array() });
@@ -20,7 +19,7 @@ const validate = (validations) => {
 };
 exports.validate = validate;
 exports.signupValidator = [
-    (0, express_validator_1.body)("name").notEmpty().withMessage("Name is required"),
+    (0, express_validator_1.body)("username").notEmpty().withMessage("Name is required"),
     (0, express_validator_1.body)("email").trim().isEmail().notEmpty().withMessage("Email is required"),
     (0, express_validator_1.body)("password").trim().isLength({ min: 6 }).withMessage("Password should contain atleast 6 characters"),
 ];
--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -4,13 +4,13 @@ import { body, ValidationChain, validationResult } from "express-validator"
 export const validate = (validations: ValidationChain[]) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         for (let validation of validations) {
-            const result = await validation.run(Request);
+            const result = await validation.run(req);
             if (!result.isEmpty()) {
                 break
             }
         }
         const errors = validationResult(req);
-        if (errors.isEmpty) {
+        if (errors.isEmpty()) {
             return next()
         }
         return res.status(422).json({ errors: errors.array() })
@@ -18,7 +18,7 @@ export const validate = (validations: ValidationChain[]) => {
 }
 
 export const signupValidator = [
-    body("name").notEmpty().withMessage("Name is required"),
+    body("username").notEmpty().withMessage("Name is required"),
     body("email").trim().isEmail().notEmpty().withMessage("Email is required"),
     body("password").trim().isLength({ min: 6 }).withMessage("Password should contain atleast 6 characters"),
 ]
