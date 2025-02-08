00c79a5b1684e0fa6ab3556c177eff813d6173ea
Gambioto <gerafeve@gmail.com>
2025-02-08 12:53:33 -0300

    debuggeando token

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -9,8 +9,8 @@ export const createToken = (id: string, email: string, expiresIn) => {
 }
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.signedCookies)
     const token = req.signedCookies[`${COOKIE_NAME}`]
+    console.log(token)
     if (!token || token.trim() === '') {
         return res.status(401).json({ message: 'Token Not Received' })
     }
