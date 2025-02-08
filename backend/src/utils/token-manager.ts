7fed38451825197ef698b23a1ef99846c3073adc
Gambioto <gerafeve@gmail.com>
2025-02-08 14:18:16 -0300

    otra correccion

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -10,7 +10,6 @@ export const createToken = (id: string, email: string, expiresIn) => {
 
 export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.signedCookies[`${COOKIE_NAME}`]
-    console.log(token)
     if (!token || token.trim() === '') {
         return res.status(401).json({ message: 'Token Not Received' })
     }
