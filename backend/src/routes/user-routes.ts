1328eb3e439e361d9c9abf48785ff1c58551811a
Gambioto <gerafeve@gmail.com>
2025-02-08 18:16:02 -0300

    se quita verify user

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -8,7 +8,7 @@ const userRoutes = Router()
 userRoutes.get("/", getAllusers)
 userRoutes.post("/signup", validate(signupValidator), userSignup)
 userRoutes.post("/login", validate(loginValidator), userLogin)
-userRoutes.get("/auth-status", verifyUser)
+userRoutes.get("/auth-status")
 userRoutes.get("/logout", logoutUser)
 
 export default userRoutes
